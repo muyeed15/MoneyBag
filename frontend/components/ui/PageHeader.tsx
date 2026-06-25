ac9235cab6c2ab453593c41ffef639f2501d2b33
muyeed15 <muyeed.al.abdullah@gmail.com>
2026-06-25 14:51:11 +0600

    refactor: update page header component

--- frontend/components/ui/PageHeader.tsx
+++ frontend/components/ui/PageHeader.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 import { BackButton } from "./BackButton";
 
 type Props = {
-  title: string;
+  title?: string;
   subtitle?: string;
   showBack?: boolean;
   children?: ReactNode;
